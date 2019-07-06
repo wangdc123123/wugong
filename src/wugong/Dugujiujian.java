2c32f68495eec9d5b2b447b0e09dc8869d066a04
wangdechun123 <1780276042@qq.com>
2019-07-06 22:17:34 +0800

    55555

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -5,6 +5,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("111111");
 		System.out.println("222222");
+		System.out.println("333333");
+		System.out.println("333333");
 	}
 
 }

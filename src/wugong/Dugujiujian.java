cc87ae0088bb7555a17bc83c1e42db6bb6b1c85c
linghuchong <1941429231@qq.com>
2019-07-06 22:24:12 +0800

    yyyyyy

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -6,7 +6,8 @@ public class Dugujiujian {
 		System.out.println("111111");
 		System.out.println("222222");
 		System.out.println("333333");
-		System.out.println("333333");
+		System.out.println("444444");
+		System.out.println("YYYYYY");
 	}
 
 }
